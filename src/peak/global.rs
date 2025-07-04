f69b047adf308a96145fa96e4aacc22f74df4e80
Augusto F. Hack <hack.augusto@gmail.com>
2025-07-04 13:46:28 +0200

    feat: additional metrics for global tracker

--- src/peak/global.rs
+++ src/peak/global.rs
@@ -7,8 +7,14 @@ use std::{
 /// Atomic used to track the maximum amount of memory allocated by the program.
 static GLOBAL_PEAK: AtomicUsize = AtomicUsize::new(0);
 
-/// Atomic used to track the current amount of memory allocated.
-static IN_USE: AtomicUsize = AtomicUsize::new(0);
+/// Atomic used to track the number of allocations
+static ALLOC_CALLS: AtomicUsize = AtomicUsize::new(0);
+
+/// Atomic used to track total number of bytes allocated
+static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
+
+/// Atomic used to track total number of bytes deallocated
+static DEALLOCATED: AtomicUsize = AtomicUsize::new(0);
 
 /// Returns the peak bytes usage for the whole program.
 pub fn peak() -> usize {
@@ -20,7 +26,26 @@ pub fn peak() -> usize {
 /// This does not take into account padding added by the underlying allocator,
 /// it only accounts for the memory requested and used by the application.
 pub fn in_use() -> usize {
-    IN_USE.load(Ordering::Relaxed)
+    // Saturaing sub because this read is racing with the alloc/dealloc calls
+    allocated().saturating_sub(deallocated())
+}
+
+/// Returns the current number of alloc calls.
+pub fn alloc_calls() -> usize {
+    ALLOC_CALLS.load(Ordering::Relaxed)
+}
+
+/// Returns the current number of bytes allocated.
+///
+/// This does not take into account padding added by the underlying allocator,
+/// it only accounts for the memory requested and used by the application.
+pub fn allocated() -> usize {
+    ALLOCATED.load(Ordering::Relaxed)
+}
+
+/// Returns the current number of bytes deallocated.
+pub fn deallocated() -> usize {
+    DEALLOCATED.load(Ordering::Relaxed)
 }
 
 /// Resets the global peak to the current memory in use.
@@ -58,6 +83,21 @@ impl<T> GlobalPeakTracker<T> {
         peak()
     }
 
+    /// Returns the current number of alloc calls.
+    pub fn alloc_calls() -> usize {
+        alloc_calls()
+    }
+
+    /// Returns the current number of bytes allocated.
+    pub fn allocated() -> usize {
+        allocated()
+    }
+
+    /// Returns the current number of bytes deallocated.
+    pub fn deallocated() -> usize {
+        deallocated()
+    }
+
     /// Returns the current memory usage.
     pub fn in_use(&self) -> usize {
         in_use()
@@ -69,9 +109,11 @@ unsafe impl<T: GlobalAlloc> GlobalAlloc for GlobalPeakTracker<T> {
         // NOTE: The code below doesn't need to use the `StateGuard` because it does no allocations
         let ret = unsafe { self.inner.alloc(layout) };
 
+        ALLOC_CALLS.fetch_add(1, Ordering::Relaxed);
+
         if !ret.is_null() {
             let size = layout.size();
-            IN_USE.fetch_add(size, Ordering::Relaxed);
+            ALLOCATED.fetch_add(size, Ordering::Relaxed);
             GLOBAL_PEAK.fetch_max(in_use(), Ordering::Relaxed);
         }
         ret
@@ -83,6 +125,6 @@ unsafe impl<T: GlobalAlloc> GlobalAlloc for GlobalPeakTracker<T> {
             self.inner.dealloc(ptr, layout);
         }
         let size = layout.size();
-        IN_USE.fetch_sub(size, Ordering::Relaxed);
+        DEALLOCATED.fetch_add(size, Ordering::Relaxed);
     }
 }
